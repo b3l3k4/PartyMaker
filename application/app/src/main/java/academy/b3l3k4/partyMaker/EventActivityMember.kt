846cec29c48b79ee7baf1387b2da2338d56a16d4
knuckl35 <karolis70012@gmail.com>
2020-01-14 10:41:45 +0200

    last update

--- application/app/src/main/java/academy/b3l3k4/partyMaker/EventActivityMember.kt
+++ application/app/src/main/java/academy/b3l3k4/partyMaker/EventActivityMember.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -48,11 +49,25 @@ class EventActivityMember: AppCompatActivity() {
 
         val attendantsList: ListView = findViewById(R.id.attendantsView)
         val eventUid = intent.getStringExtra("uid")
+        val userId = FirebaseAuth.getInstance().currentUser!!.uid
 
         readData(eventUid, object: FirebaseCallback {
             override fun onCalback(list: MutableList<Member>) {
                 val adapter = CustomMemberListAdapter(this@EventActivityMember, R.layout.custom_member_list, list)
                 attendantsList.adapter = adapter
+            }
+        })
+
+        //leave party
+        val leaveParty: TextView = findViewById(R.id.leaveParty)
+
+        leaveParty.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(v: View?) {
+                val eventReference = FirebaseDatabase.getInstance().getReference("Events").child(userId).child(eventUid!!)
+                val memberReference = FirebaseDatabase.getInstance().getReference("EventMembers").child(eventUid).child(userId)
+                eventReference.removeValue()
+                memberReference.removeValue()
+                startActivity(Intent(this@EventActivityMember, MainScreen::class.java))
             }
         })
 
